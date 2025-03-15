72a9b0d38f1310cfe8468cff2088620635021e99
DeepakSharma09 <deepakgaur.dg69@gmail.com>
2025-03-15 17:44:52 +0530

    adding _new_branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-import 'package:uthix_app/UpcomingPage.dart';
+import 'package:uthix_app/upcoming.dart';
 import 'package:uthix_app/view/Ecommerce/e_commerce.dart';
 import 'package:uthix_app/view/Seller_dashboard/dashboard.dart';
+import 'package:uthix_app/view/Student_Pages/Buy_Books/BookDetails.dart';
 import 'package:uthix_app/view/Student_Pages/HomePages/HomePage.dart';
 import 'package:uthix_app/view/Student_Pages/LMS/query_provider.dart';
 import 'package:uthix_app/view/homeRegistration/splashintroScreen.dart';
 import 'package:uthix_app/view/instructor_dashboard/Class/announcement.dart';
+import 'package:uthix_app/view/instructor_dashboard/Class/live_classes.dart';
 import 'package:uthix_app/view/instructor_dashboard/Class/new_announcement.dart';
 import 'package:uthix_app/view/instructor_dashboard/calender/calender.dart';
 import 'package:uthix_app/view/instructor_dashboard/Dashboard/instructor_dashboard.dart';
@@ -38,9 +40,12 @@ class MainApp extends StatelessWidget {
       ),
       //home: StartLogin()
       home: Introscreen(),
+      // home: UnderConstructionScreen(),
       //home: SellerDashboard(),
       //home: InstructorDashboard(),
       //home: ECommerce(),
+      //home: LiveClasses(),
+      // home: Bookdetails(product: null,),
     );
   }
 }
