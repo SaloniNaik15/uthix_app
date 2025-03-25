16434cbb58b0cf10d7475974a09825eba3186cc5
DeepakSharma09 <deepakgaur.dg69@gmail.com>
2025-03-25 14:37:48 +0530

    routing

--- lib/Logout.dart
+++ lib/Logout.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:uthix_app/view/login/email_id.dart';
@@ -16,7 +15,7 @@ Future<void> logoutUser(BuildContext context) async {
           ElevatedButton(
             style: ElevatedButton.styleFrom(
               backgroundColor: Color(0xFF1B617A), // Background color
-              foregroundColor: Colors.white,     // Text color
+              foregroundColor: Colors.white, // Text color
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(8),
               ),
@@ -29,24 +28,23 @@ Future<void> logoutUser(BuildContext context) async {
           ElevatedButton(
             style: ElevatedButton.styleFrom(
               backgroundColor: Colors.red, // Background color
-              foregroundColor: Colors.white,     // Text color
+              foregroundColor: Colors.white, // Text color
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(8),
               ),
             ),
             child: Text("Yes, Logout"),
-
             onPressed: () async {
               Navigator.of(context).pop(); // Close the dialog first
 
               SharedPreferences prefs = await SharedPreferences.getInstance();
               await prefs.remove('auth_token');
               await prefs.remove('user_role');
-
+//used for testing
               Navigator.pushAndRemoveUntil(
                 context,
                 MaterialPageRoute(builder: (context) => EmailId()),
-                    (Route<dynamic> route) => false,
+                (Route<dynamic> route) => false,
               );
             },
           ),
